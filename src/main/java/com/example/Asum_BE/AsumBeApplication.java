3aa13fdad1f400ec24dc1394719868030392ebe6
Ji Eunyoung <jeunyeong323@gmail.com>
2025-02-25 14:37:20 +0900

    first commit

---
+++ src/main/java/com/example/Asum_BE/AsumBeApplication.java
@@ -0,0 +1,15 @@
+package com.example.Asum_BE;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.example.Asum_BE")
+public class AsumBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsumBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Asum_BE/AsumBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Asum_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsumBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
